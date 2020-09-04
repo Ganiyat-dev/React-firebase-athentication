e4632d81bddbc4ea6dbf1e38dfb80b0ddf253d9a
Ganiyat-dev <ganiyatolajumoke.go@gmail.com>
2020-09-04 05:39:30 +0100

    Created a Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Login from "./components/Login";
+import Register from "./components/Register";
+// function App() {
+//   return (
+//     <div className="App">
+//       <Login Name="Ganiyat" />
+//       <Register FirstName="Azeez" />
+//     </div>
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const name = "Larry";
+
+    return (
+      <div className="App">
+        <h1> The App Component</h1>
+        <h4>Hello{name}</h4>
+        <h4> 1+1 = {1 + 1} </h4>
+        <Login />
+        <Register />
+      </div>
+    );
+  }
 }
-
 export default App;
